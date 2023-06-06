599372f92f9e8acb64bff48c3a3bc7021a434c15
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-06-06 15:07:37 +0300

    add dark for accordion

--- plugins/primeVue.js
+++ plugins/primeVue.js
@@ -138,23 +138,20 @@ const tailwindLight = {
         root: {
             class: ['mb-1']
         },
-        header: ({ props }) => {
-            console.log(props);
-
-            return {
-                class: [
-                    { 'select-none pointer-events-none cursor-default opacity-60': props?.disabled } // condition
-                ]
-            };
-        },
+        header: ({ props }) => ({
+            class: [
+                { 'select-none pointer-events-none cursor-default opacity-60': props?.disabled } // Condition
+            ]
+        }),
         headerAction: ({ context }) => ({
             class: [
-                'flex items-center cursor-pointer relative no-underline select-none', // alignments
-                'p-5 transition duration-200 ease-in-out rounded-t-md font-bold transition-shadow duration-200', // padding and transition
-                'border border-gray-300 bg-gray-100 text-gray-600', // borders and colors
-                'hover:border-gray-300 hover:bg-gray-200 hover:text-gray-800', // hover
-                'focus:outline-none focus:outline-offset-0 focus:shadow-[inset_0_0_0_0.2rem_rgba(191,219,254,1)]', // focus
-                { 'rounded-br-md rounded-bl-md': !context.active, 'rounded-br-0 rounded-bl-0 text-gray-800': context.active } // condition
+                'flex items-center cursor-pointer relative no-underline select-none', // Alignments
+                'p-5 transition duration-200 ease-in-out rounded-t-md font-bold transition-shadow duration-200', // Padding and transition
+                'border border-gray-300 bg-gray-100 text-gray-600', // Borders and colors
+                'dark:bg-gray-900 dark:border-blue-900/40 dark:text-white/80 dark:hover:bg-gray-900/80 dark:focus:shadow-[inset_0_0_0_0.2rem_rgba(147,197,253,0.5)]', // Dark mode
+                'hover:border-gray-300 hover:bg-gray-200 hover:text-gray-800', // Hover
+                'focus:outline-none focus:outline-offset-0 focus:shadow-[inset_0_0_0_0.2rem_rgba(191,219,254,1)]', // Focus
+                { 'rounded-br-md rounded-bl-md': !context.active, 'rounded-br-0 rounded-bl-0 text-gray-800': context.active } // Condition
             ]
         }),
         headerIcon: {
@@ -164,14 +161,17 @@ const tailwindLight = {
             class: ['leading-none']
         },
         content: {
-            class: ['p-5 border border-gray-300 bg-white text-gray-700 rounded-tl-none rounded-tr-none rounded-br-lg rounded-bl-lg']
+            class: [
+                'p-5 border border-gray-300 bg-white text-gray-700 border-t-0 rounded-tl-none rounded-tr-none rounded-br-lg rounded-bl-lg',
+                'dark:bg-gray-900 dark:border-blue-900/40 dark:text-white/80' // Dark mode
+            ]
         }
     },
     card: {
         root: {
             class: [
                 'bg-white text-gray-700 shadow-md rounded-md', // Background, text color, box shadow, and border radius.
-                'dark:bg-gray-800 dark:text-white ' //dark
+                'dark:bg-gray-900 dark:text-white ' //dark
             ]
         },
         body: {
