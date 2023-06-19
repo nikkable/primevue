1aeb12877e2ceed23be6812526a0d93cca5a638f
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-06-19 10:38:50 +0300

    finalize buttons unstyled

--- plugins/primeVue.js
+++ plugins/primeVue.js
@@ -470,22 +470,52 @@ const tailwindLight = {
     //BUTTONS
 
     button: {
-        root: ({ props }) => ({
+        root: ({ props, disabled }) => ({
             class: [
                 console.log(props),
                 'items-center cursor-pointer inline-flex overflow-hidden relative select-none text-center align-bottom',
-                'px-4 py-3 text-base  transition duration-200 ease-in-out',
-                { 'text-white bg-blue-500 border border-blue-500 hover:bg-blue-600 hover:border-blue-600': !props.link && props.severity == null, 'text-blue-600 bg-transparent border-transparent': props.link },
+                'transition duration-200 ease-in-out',
                 {
-                    'text-white bg-gray-500 border border-gray-500 hover:bg-gray-600 hover:border-gray-600': props.severity == 'secondary',
-                    'text-white bg-green-500 border border-green-500 hover:bg-green-600 hover:border-green-600': props.severity == 'success',
-                    'text-white bg-blue-500 border border-blue-500 hover:bg-blue-600 hover:border-blue-600': props.severity == 'info',
-                    'text-white bg-orange-500 border border-orange-500 hover:bg-orange-600 hover:border-orange-600': props.severity == 'warning',
-                    'text-white bg-purple-500 border border-purple-500 hover:bg-purple-600 hover:border-purple-600': props.severity == 'help',
-                    'text-white bg-red-500 border border-red-500 hover:bg-red-600 hover:border-red-600': props.severity == 'danger'
+                    'text-white bg-blue-500 border border-blue-500 hover:bg-blue-600 hover:border-blue-600': !props.link && props.severity == null && !props.text && !props.outlined && !props.plain,
+                    'text-blue-600 bg-transparent border-transparent': props.link
+                },
+                {
+                    'text-white bg-gray-500 border border-gray-500 hover:bg-gray-600 hover:border-gray-600': props.severity == 'secondary' && !props.text && !props.outlined && !props.plain,
+                    'text-white bg-green-500 border border-green-500 hover:bg-green-600 hover:border-green-600': props.severity == 'success' && !props.text && !props.outlined && !props.plain,
+                    'text-white bg-blue-500 border border-blue-500 hover:bg-blue-600 hover:border-blue-600': props.severity == 'info' && !props.text && !props.outlined && !props.plain,
+                    'text-white bg-orange-500 border border-orange-500 hover:bg-orange-600 hover:border-orange-600': props.severity == 'warning' && !props.text && !props.outlined && !props.plain,
+                    'text-white bg-purple-500 border border-purple-500 hover:bg-purple-600 hover:border-purple-600': props.severity == 'help' && !props.text && !props.outlined && !props.plain,
+                    'text-white bg-red-500 border border-red-500 hover:bg-red-600 hover:border-red-600': props.severity == 'danger' && !props.text && !props.outlined && !props.plain
                 },
                 { 'shadow-lg': props.raised },
-                { 'rounded-md': !props.rounded, 'rounded-full': props.rounded }
+                { 'rounded-md': !props.rounded, 'rounded-full': props.rounded },
+                {
+                    'bg-transparent border-transparent': props.text && !props.plain,
+                    'text-blue-500 hover:bg-blue-300/20': props.text && props.severity == null && !props.plain,
+                    'text-gray-500 hover:bg-gray-300/20': props.text && props.severity == 'secondary' && !props.plain,
+                    'text-green-500 hover:bg-green-300/20': props.text && props.severity == 'success' && !props.plain,
+                    'text-blue-500 hover:bg-blue-300/20': props.text && props.severity == 'info' && !props.plain,
+                    'text-orange-500 hover:bg-orange-300/20': props.text && props.severity == 'warning' && !props.plain,
+                    'text-purple-500 hover:bg-purple-300/20': props.text && props.severity == 'help' && !props.plain,
+                    'text-red-500 hover:bg-red-300/20': props.text && props.severity == 'danger' && !props.plain
+                },
+                { 'shadow-lg': props.raised && props.text },
+                {
+                    'text-gray-500 hover:bg-gray-300/20': props.plain & props.text,
+                    'text-gray-500 border border-gray-500 hover:bg-gray-300/20': props.plain & props.outlined,
+                    'text-white bg-gray-500 border border-gray-500 hover:bg-gray-600 hover:border-gray-600': props.plain & !props.outlined & !props.text
+                },
+                {
+                    'bg-transparent border': props.outlined && !props.plain,
+                    'text-blue-500 border border-blue-500 hover:bg-blue-300/20': props.outlined && props.severity == null && !props.plain,
+                    'text-gray-500 border border-gray-500 hover:bg-gray-300/20': props.outlined && props.severity == 'secondary' && !props.plain,
+                    'text-green-500 border border-green-500 hover:bg-green-300/20': props.outlined && props.severity == 'success' && !props.plain,
+                    'text-blue-500 border border-blue-500 hover:bg-blue-300/20': props.outlined && props.severity == 'info' && !props.plain,
+                    'text-orange-500 border border-orange-500 hover:bg-orange-300/20': props.outlined && props.severity == 'warning' && !props.plain,
+                    'text-purple-500 border border-purple-500 hover:bg-purple-300/20': props.outlined && props.severity == 'help' && !props.plain,
+                    'text-red-500 border border-red-500 hover:bg-red-300/20': props.outlined && props.severity == 'danger' && !props.plain
+                },
+                { 'px-4 py-3 text-base': props.size == null, 'text-xs py-2 px-3': props.size == 'small', 'text-xl py-3 px-4': props.size == 'large' }
             ]
         }),
         label: ({ props }) => ({
@@ -496,6 +526,9 @@ const tailwindLight = {
                 'mx-0',
                 { 'mr-2': props.iconPos == 'left' && props.label != null, 'ml-2': props.iconPos == 'right' && props.label != null, 'mb-2': props.iconPos == 'top' && props.label != null, 'mt-2': props.iconPos == 'bottom' && props.label != null }
             ]
+        }),
+        badge: ({ props }) => ({
+            class: [{ 'ml-2 w-4 h-4 leading-none': props.badge }]
         })
     }
 };
