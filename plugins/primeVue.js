6ed752f5831009d51ffd2813f184216193f0020b
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-06-19 12:42:33 +0300

    add styles for badges

--- plugins/primeVue.js
+++ plugins/primeVue.js
@@ -470,9 +470,8 @@ const tailwindLight = {
     //BUTTONS
 
     button: {
-        root: ({ props, disabled }) => ({
+        root: ({ props }) => ({
             class: [
-                console.log(props),
                 'items-center cursor-pointer inline-flex overflow-hidden relative select-none text-center align-bottom',
                 'transition duration-200 ease-in-out',
                 {
@@ -529,6 +528,27 @@ const tailwindLight = {
         }),
         badge: ({ props }) => ({
             class: [{ 'ml-2 w-4 h-4 leading-none': props.badge }]
+        })
+    },
+
+    //MISC
+
+    badge: {
+        root: ({ props }) => ({
+            class: [
+                console.log(props),
+                'rounded-full p-0 text-center inline-block',
+                'bg-blue-500 text-white font-bold',
+                {
+                    'bg-gray-500 ': props.severity == 'secondary',
+                    'bg-green-500 ': props.severity == 'success',
+                    'bg-blue-500 ': props.severity == 'info',
+                    'bg-orange-500 ': props.severity == 'warning',
+                    'bg-purple-500 ': props.severity == 'help',
+                    'bg-red-500 ': props.severity == 'danger'
+                },
+                { 'text-xs min-w-[1.5rem] h-[1.5rem] leading-[1.5rem]': props.size == null, 'text-lg min-w-[2.25rem] h-[2.25rem] leading-[2.25rem]': props.size == 'large', 'text-2xl min-w-[3rem] h-[3rem] leading-[3rem]': props.size == 'xlarge' }
+            ]
         })
     }
 };
